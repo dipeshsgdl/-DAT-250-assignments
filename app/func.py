24283d14196d9eafd435cab83f542702ec6a5f96
ferrs88 <5775130+Ferrs88@users.noreply.github.com>
2022-09-23 14:21:21 +0200

    escape special symbols

--- app/func.py
+++ app/func.py
@@ -7,3 +7,13 @@ def hash_password(plain_text_password):
 
 def check_password(plain_text_password, hashed_password):
     return bcrypt.checkpw(base64.b64encode(hashlib.sha256(plain_text_password.encode('utf-8')).digest()), hashed_password)
+
+def safe_convert(input):
+    try:
+      number = int(input)
+      return number
+    except:
+      return quote(input)
+
+def convert_back(input):
+    return unquote(input)
