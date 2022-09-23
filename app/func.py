800840b49623ce0b9335c323bbc9245ac4aba5e2
ferrs88 <5775130+Ferrs88@users.noreply.github.com>
2022-09-23 14:24:41 +0200

    update

--- app/func.py
+++ app/func.py
@@ -8,6 +8,7 @@ def hash_password(plain_text_password):
 def check_password(plain_text_password, hashed_password):
     return bcrypt.checkpw(base64.b64encode(hashlib.sha256(plain_text_password.encode('utf-8')).digest()), hashed_password)
 
+#Escape special symbols.
 def safe_convert(input):
     try:
       number = int(input)
