91173fe06d1c190f133e8c5db720390682c85646
ferrs88 <5775130+Ferrs88@users.noreply.github.com>
2022-09-25 13:47:52 +0200

    only allowed files can be uploaded

--- app/func.py
+++ app/func.py
@@ -1,6 +1,6 @@
 import bcrypt,base64,hashlib
 from urllib.parse import unquote,quote
-from app import query_db
+from app import app,query_db
 
 # Hash
 def hash_password(plain_text_password):
@@ -13,16 +13,20 @@ def check_password(plain_text_password, hashed_password):
 
 # Escape special symbols.
 def safe_convert(input):
+    if input is None: return input
     try:
-      number = int(input)
-      return number
+      return int(input)
     except:
       return quote(input)
 
 def convert_back(input):
+    if input is None: return input
     return unquote(input)
 
 def check_if_username_exist(username_entered):
     existing_user = query_db('SELECT * FROM Users WHERE username="{}" limit 1;'.format(username_entered), one=True)
     if existing_user is None: return False
     return False if existing_user['username'] is None else True
+
+def allowed_file(file):
+    return '.' in file and file.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,8 @@ from app import app,query_db,login_manager
 from app.forms import IndexForm, PostForm, FriendsForm, ProfileForm, CommentsForm
 from datetime import datetime
 import os, flask_login
-from app.func import check_if_username_exist,hash_password,check_password,safe_convert,convert_back
+from app.func import check_if_username_exist,hash_password,check_password,safe_convert,convert_back,allowed_file
+from werkzeug.utils import secure_filename
 
 # this file contains all the different routes, and the logic for communicating with the database
 
@@ -41,12 +42,11 @@ def index():
             flash('Username and/or Password incorrect')
 
     elif form.register.is_submitted() and form.register.submit.data:
-        username_entered = safe_convert(form.register.username.data)
-        existing_username = check_if_username_exist(username_entered)
-        if existing_username:
+        username = safe_convert(form.register.username.data)
+        if check_if_username_exist(username):
             flash('Username not available')
         else:
-            query_db('INSERT INTO Users (username, first_name, last_name, password) VALUES("{}", "{}", "{}", "{}");'.format(username_entered,safe_convert(form.register.first_name.data),
+            query_db('INSERT INTO Users (username, first_name, last_name, password) VALUES("{}", "{}", "{}", "{}");'.format(username,safe_convert(form.register.first_name.data),
             safe_convert(form.register.last_name.data), hash_password(form.register.password.data)))
             return redirect(url_for('index'))
     return render_template('index.html', title='Welcome', form=form)
@@ -62,9 +62,12 @@ def stream(username):
     user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
     if form.is_submitted():
         if form.image.data:
-            path = os.path.join(app.config['UPLOAD_PATH'], form.image.data.filename)
-            form.image.data.save(path)
-
+            if allowed_file(form.image.data.filename):
+                path = os.path.join(app.config['UPLOAD_PATH'], secure_filename(form.image.data.filename))
+                form.image.data.save(path)
+            else:
+                flash('File type is not supported!', 'error')
+                return redirect(url_for('stream', username=username))
 
         query_db('INSERT INTO Posts (u_id, content, image, creation_time) VALUES({}, "{}", "{}", \'{}\');'.format(user['id'], form.content.data, form.image.data.filename, datetime.now()))
         return redirect(url_for('stream', username=username))
@@ -82,6 +85,7 @@ def comments(username, p_id):
     if form.is_submitted():
         user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
         query_db('INSERT INTO Comments (p_id, u_id, comment, creation_time) VALUES({}, {}, "{}", \'{}\');'.format(p_id, user['id'], safe_convert(form.comment.data), datetime.now()))
+        return redirect(url_for('comments', username=username, p_id=p_id))
 
     post = query_db('SELECT * FROM Posts WHERE id={};'.format(p_id), one=True)
     all_comments = query_db('SELECT DISTINCT creation_time, comment, username FROM Comments AS c JOIN Users AS u ON c.u_id=u.id WHERE c.p_id={} ORDER BY c.creation_time DESC;'.format(p_id))
@@ -101,7 +105,7 @@ def friends(username):
     if form.is_submitted():
         friend = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.username.data), one=True)
         if friend is None:
-            flash('User does not exist')
+            flash('User does not exist', 'error')
         else:
             query_db('INSERT INTO Friends (u_id, f_id) VALUES({}, {});'.format(user['id'], friend['id']))
 
@@ -116,11 +120,12 @@ def profile(username):
     form = ProfileForm()
     if form.is_submitted():
         query_db('UPDATE Users SET education="{}", employment="{}", music="{}", movie="{}", nationality="{}", birthday=\'{}\' WHERE username="{}" ;'.format(
-            form.education.data, form.employment.data, form.music.data, form.movie.data, form.nationality.data, form.birthday.data, username
+            safe_convert(form.education.data), safe_convert(form.employment.data), safe_convert(form.music.data), safe_convert(form.movie.data), safe_convert(form.nationality.data), safe_convert(form.birthday.data), username
         ))
         return redirect(url_for('profile', username=username))
 
     user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
+    # TODO: Convert back to normal string
     return render_template('profile.html', title='profile', username=username, user=user, form=form)
 
 @app.route('/logout')
